6375e56b861b4cd252d2c747a047b260bc7460ed
Reema <reema.sh2211@gmail.com>
2021-07-08 08:23:22 +0300

    fixing

--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ from flask import Flask, jsonify
 def create_app(test_config=None):
  # Create and configure the app
  # Include the first parameter: Here, __name__is the name of the current Python module.
- app = Flask(__name__)
+ app = Flask(__name__) 
+ 
 
  # Return the app instance
  return app
